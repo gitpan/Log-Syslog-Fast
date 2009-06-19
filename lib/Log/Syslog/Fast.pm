1cde75e57de24f8080be0c77e9fb4e64dd56c071
Adam Thomason <athomason@cpan.org>
2009-06-19 03:08:08 +0000

    Import of ATHOMASON/Log-Syslog-Fast-0.15 from CPAN.
    gitpan-cpan-distribution: Log-Syslog-Fast
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         ATHOMASON/Log-Syslog-Fast-0.15.tar.gz
    gitpan-cpan-author:       ATHOMASON
    gitpan-cpan-maturity:     released

---
+++ lib/Log/Syslog/Fast.pm
@@ -0,0 +1,202 @@
+package Log::Syslog::Fast;
+
+use 5.008005;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+use constant {
+    # protocols
+    LOG_UDP         => 0, # UDP
+    LOG_TCP         => 1, # TCP
+
+    # severities
+    LOG_EMERG       => 0, # system is unusable
+    LOG_ALERT       => 1, # action must be taken immediately
+    LOG_CRIT        => 2, # critical conditions
+    LOG_ERR         => 3, # error conditions
+    LOG_WARNING     => 4, # warning conditions
+    LOG_NOTICE      => 5, # normal but significant condition
+    LOG_INFO        => 6, # informational
+    LOG_DEBUG       => 7, # debug-level messages
+
+    # facilities
+    LOG_KERN        => 0, # kernel messages
+    LOG_USER        => 1, # random user-level messages
+    LOG_MAIL        => 2, # mail system
+    LOG_DAEMON      => 3, # system daemons
+    LOG_AUTH        => 4, # security/authorization messages
+    LOG_SYSLOG      => 5, # messages generated internally by syslogd
+    LOG_LPR         => 6, # line printer subsystem
+    LOG_NEWS        => 7, # network news subsystem
+    LOG_UUCP        => 8, # UUCP subsystem
+    LOG_CRON        => 9, # clock daemon
+    LOG_AUTHPRIV    => 10, # security/authorization messages (private)
+    LOG_FTP         => 11, # ftp daemon
+    LOG_LOCAL0      => 16, # reserved for local use
+    LOG_LOCAL1      => 17, # reserved for local use
+    LOG_LOCAL2      => 18, # reserved for local use
+    LOG_LOCAL3      => 19, # reserved for local use
+    LOG_LOCAL4      => 20, # reserved for local use
+    LOG_LOCAL5      => 21, # reserved for local use
+    LOG_LOCAL6      => 22, # reserved for local use
+    LOG_LOCAL7      => 23, # reserved for local use
+};
+
+our %EXPORT_TAGS = (
+    protos   => [qw/
+        LOG_TCP LOG_UDP
+    /],
+    facilities   => [qw/
+        LOG_EMERG LOG_ALERT LOG_CRIT LOG_ERR LOG_WARNING
+        LOG_NOTICE LOG_INFO LOG_DEBUG
+    /],
+    severities => [qw/
+        LOG_KERN LOG_USER LOG_MAIL LOG_DAEMON LOG_AUTH
+        LOG_SYSLOG LOG_LPR LOG_NEWS LOG_UUCP LOG_CRON
+        LOG_AUTHPRIV LOG_FTP LOG_LOCAL0 LOG_LOCAL1 LOG_LOCAL2
+        LOG_LOCAL3 LOG_LOCAL4 LOG_LOCAL5 LOG_LOCAL6 LOG_LOCAL7
+    /],
+);
+@{ $EXPORT_TAGS{'all'} } = (
+    @{ $EXPORT_TAGS{'protos'} },
+    @{ $EXPORT_TAGS{'facilities'} },
+    @{ $EXPORT_TAGS{'severities'} }
+);
+
+our @EXPORT_OK = @{ $EXPORT_TAGS{'all'} };
+our @EXPORT = qw();
+
+our $VERSION = '0.15';
+
+require XSLoader;
+XSLoader::load('Log::Syslog::Fast', $VERSION);
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Syslog::Fast - Perl extension for very quickly sending syslog messages over TCP or UDP.
+
+=head1 SYNOPSIS
+
+  use Log::Syslog::Fast;
+  my $logger = Log::Syslog::Fast->new(LOG_UDP, "127.0.0.1", 514, 16, 6, "mymachine", "logger");
+  $logger->send("log message", time);
+
+=head1 DESCRIPTION
+
+This module sends syslog messages over a network socket. It works like
+L<Sys::Syslog> in setlogsock's 'udp' or 'tcp' modes, but without the
+significant CPU overhead of that module when used for high-volume logging. Use
+of this specialized module is only necessary if 1) you must use network syslog
+as a messaging transport but 2) need to minimize the time spent in the logger.
+
+This module supercedes the less general L<Log::Syslog::UDP>.
+
+=head1 METHODS
+
+=over 4
+
+=item Log::Syslog::Fast-E<gt>new($proto, $hostname, $port, $facility, $severity, $sender, $name);
+
+Create a new Log::Syslog::Fast object with the following parameters:
+
+=over 4
+
+=item $proto
+
+The transport protocol, either LOG_TCP or LOG_UDP.
+
+If LOG_TCP is used, calls to $logger-E<gt>send() will block until remote
+receipt of the message is confirmed. If LOG_UDP is used, the call will never
+block and may fail if insufficient buffer space exists in the network stack.
+
+=item $hostname
+
+The destination hostname where a syslogd is running.
+
+=item $port
+
+The destination port where a syslogd is listening. Usually 514.
+
+=item $facility
+
+The syslog facility constant, eg 16 for 'local0'. See RFC3164 section 4.1.1 (or
+E<lt>sys/syslog.hE<gt>) for appropriate constant values.
+
+=item $severity
+
+The syslog severity constant, eg 6 for 'info'. See RFC3164 section 4.1.1 (or
+E<lt>sys/syslog.hE<gt>) for appropriate constant values.
+
+=item $sender
+
+The originating hostname. Sys::Hostname::hostname is typically a reasonable
+source for this.
+
+=item $name
+
+The program name or tag to use for the message.
+
+=back
+
+=item $logger-E<gt>send($logmsg, [$time])
+
+Send a syslog message through the configured logger. If $time is not provided,
+CORE::time() will be called for you. That doubles the syscalls per message, so
+try to pass it if you're already calling time() yourself.
+
+=item $logger-E<gt>set_receiver($hostname, $port)
+
+Change the destination host and port. This will force a reconnection in LOG_TCP
+mode.
+
+=item $logger-E<gt>set_priority($facility, $severity)
+
+Change the syslog facility and severity.
+
+=item $logger-E<gt>set_sender($sender)
+
+Change what is sent as the hostname of the sender.
+
+=item $logger-E<gt>set_name($name)
+
+Change what is sent as the name of the sending program.
+
+=item $logger-E<gt>set_pid($name)
+
+Change what is sent as the process id of the sending program.
+
+=back
+
+=head1 EXPORT
+
+You may optionally import constants for severity and facility levels.
+
+  use Log::Syslog::Fast qw(:protos); # LOG_TCP and LOG_UDP
+  use Log::Syslog::Fast qw(:severities); # LOG_CRIT, LOG_NOTICE, LOG_DEBUG, etc
+  use Log::Syslog::Fast qw(:facilities); # LOG_CRON, LOG_LOCAL3, etc
+  use Log::Syslog::Fast qw(:all); # all of the above
+
+=head1 SEE ALSO
+
+L<Sys::Syslog>
+
+=head1 AUTHOR
+
+Adam Thomason, E<lt>athomason@sixapart.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008 by Six Apart, Ltd.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.5 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ t/Log-Syslog-Fast.t
@@ -0,0 +1,170 @@
+use strict;
+use warnings;
+
+use Test::More 'no_plan';
+use IO::Socket::INET;
+
+BEGIN { use_ok('Log::Syslog::Fast', ':all') };
+
+is(LOG_EMERG,    0,  'LOG_EMERG');
+is(LOG_ALERT,    1,  'LOG_ALERT');
+is(LOG_CRIT,     2,  'LOG_CRIT');
+is(LOG_ERR,      3,  'LOG_ERR');
+is(LOG_WARNING,  4,  'LOG_WARNING');
+is(LOG_NOTICE,   5,  'LOG_NOTICE');
+is(LOG_INFO,     6,  'LOG_INFO');
+is(LOG_DEBUG,    7,  'LOG_DEBUG');
+
+is(LOG_KERN,     0,  'LOG_KERN');
+is(LOG_USER,     1,  'LOG_USER');
+is(LOG_MAIL,     2,  'LOG_MAIL');
+is(LOG_DAEMON,   3,  'LOG_DAEMON');
+is(LOG_AUTH,     4,  'LOG_AUTH');
+is(LOG_SYSLOG,   5,  'LOG_SYSLOG');
+is(LOG_LPR,      6,  'LOG_LPR');
+is(LOG_NEWS,     7,  'LOG_NEWS');
+is(LOG_UUCP,     8,  'LOG_UUCP');
+is(LOG_CRON,     9,  'LOG_CRON');
+is(LOG_AUTHPRIV, 10, 'LOG_AUTHPRIV');
+is(LOG_FTP,      11, 'LOG_FTP');
+is(LOG_LOCAL0,   16, 'LOG_LOCAL0');
+is(LOG_LOCAL1,   17, 'LOG_LOCAL1');
+is(LOG_LOCAL2,   18, 'LOG_LOCAL2');
+is(LOG_LOCAL3,   19, 'LOG_LOCAL3');
+is(LOG_LOCAL4,   20, 'LOG_LOCAL4');
+is(LOG_LOCAL5,   21, 'LOG_LOCAL5');
+is(LOG_LOCAL6,   22, 'LOG_LOCAL6');
+is(LOG_LOCAL7,   23, 'LOG_LOCAL7');
+
+my $p;
+
+my $test_port = 10514;
+
+for my $proto (LOG_UDP, LOG_TCP) {
+
+    $p = ($proto == LOG_UDP ? 'udp' : 'tcp');
+
+    my $listener = listener();
+    ok($listener, "$p: listen on port $test_port");
+
+    my $logger = Log::Syslog::Fast->new($proto, "127.0.0.1", $test_port, 4, 6, "localhost", "test");
+    ok($logger, "$p: ->new returns something");
+
+    is(ref $logger, 'Log::Syslog::Fast', "$p: ->new returns a Log::Syslog::Fast object");
+
+    my $receiver = l2r($listener);
+
+    {
+        my $sent = eval { $logger->send("testing 1", time) };
+        ok(!$@, "$p: ->send with time doesn't throw");
+        is($sent, 52, "$p: ->send sent whole payload");
+
+        my $found = wait_for_readable($receiver);
+        ok($found, "$p: didn't time out while waiting for data");
+
+        if ($found) {
+            $receiver->recv(my $buf, 256);
+            is(length $buf, 52, "$p: payload is right size");
+            ok($buf =~ /^<38>/, "$p: ->send with time has the right priority");
+            ok($buf =~ /testing 1$/, "$p: ->send with time sends right payload");
+        }
+    }
+
+    {
+        my $sent = eval { $logger->send("testing 2") };
+        ok(!$@, "$p: ->send without time doesn't throw");
+        is($sent, 52, "$p: ->send sent whole payload");
+
+        my $found = wait_for_readable($receiver);
+        ok($found, "$p: didn't time out while waiting for data");
+
+        if ($found) {
+            $receiver->recv(my $buf, 256);
+            is(length $buf, 52, "$p: payload is right size");
+            ok($buf =~ /^<38>/, "$p: ->send without time has the right priority");
+            ok($buf =~ /testing 2$/, "$p: ->send without time sends right payload");
+        }
+    }
+
+    eval {
+        $test_port++;
+        $listener = listener();
+        $logger = Log::Syslog::Fast->new($proto, "127.0.0.1", $test_port, LOG_AUTH, LOG_INFO, "localhost", "test");
+
+        $listener->accept if $p eq 'tcp'; # ignore first connection
+
+        eval {
+            $logger->set_receiver($proto, "127.0.0.1", $test_port);
+        };
+        ok(!$@, "$p: ->set_receiver doesn't throw");
+
+        eval {
+            $logger->set_priority(LOG_NEWS, LOG_CRIT);
+        };
+        ok(!$@, "$p: ->set_priority doesn't throw");
+
+        eval {
+            $logger->set_sender("otherhost");
+        };
+        ok(!$@, "$p: ->set_sender doesn't throw");
+
+        eval {
+            $logger->set_name("test2");
+        };
+        ok(!$@, "$p: ->set_name doesn't throw");
+
+        eval {
+            $logger->set_pid("12345");
+        };
+        ok(!$@, "$p: ->set_name doesn't throw");
+
+        my $receiver = l2r($listener);
+
+        my $sent = eval { $logger->send("testing 3") };
+        ok(!$@, "$p: ->send after accessors doesn't throw");
+        is($sent, 53, "$p: ->send sent whole payload");
+
+        my $found = wait_for_readable($receiver);
+        ok($found, "$p: didn't time out while listening");
+
+        if ($found) {
+            $receiver->recv(my $buf, 256);
+            ok($buf, "$p: send after setReceiver went to correct port");
+            is(length $buf, 53, "$p: payload is right size");
+            ok($buf =~ /^<58>/, "$p: ->send after setPriority has the right priority");
+            ok($buf =~ /otherhost/, "$p: ->send after setSender has the right sender");
+            ok($buf =~ /test2\[/, "$p: ->send after setName has the right name");
+            ok($buf =~ /\[12345\]/, "$p: ->send after setName has the right pid");
+            ok($buf =~ /testing 3$/, "$p: ->send after accessors sends right payload");
+        }
+    };
+}
+
+sub listener {
+    return IO::Socket::INET->new(
+        Proto       => $p,
+        LocalHost   => 'localhost',
+        LocalPort   => $test_port,
+        ($p eq 'tcp' ? (Listen => 5) : ()),
+        Reuse       => 1,
+    );
+}
+
+sub l2r {
+    my $listener = shift;
+    return $listener if $p eq 'udp';
+    if ($p eq 'tcp') {
+        my $receiver = $listener->accept;
+        $receiver->blocking(0);
+        return $receiver;
+    }
+}
+
+# use select so test won't block on failure
+sub wait_for_readable {
+    my $sock = shift;
+    vec(my $rin = '', fileno($sock), 1) = 1;
+    return select(my $rout = $rin, undef, undef, 1);
+}
+
+# vim: filetype=perl
